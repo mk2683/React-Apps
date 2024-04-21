509f7c6fea47fe63326dc738378f262ee69826ee
mk2683 <mksujanian2683@gmail.com>
2024-04-21 15:01:11 +0530

    Add app to center a div using flexbox

---
+++ src/App/App.js
@@ -0,0 +1,11 @@
+import styles from './App.module.css';
+
+function App(){
+  return (
+    <div className={styles.parent}>
+      <div className={styles.child}>hello mohit here</div>
+    </div>
+  )
+}
+
+export default App;
